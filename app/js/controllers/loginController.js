def600dc96c11ed3c7efdc7cc73a002bfae37be5
Angelo Altavas <angeloaltavas@yahoo.com>
2014-09-24 01:32:20 -0700

    added auth/auth and working timer

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,24 +1,17 @@
+'use strict';
+
 module.exports = function(app) {
-  app.controller('LoginController', function($scope, $http, $base64, $cookies, $location) {
+  app.controller('LoginController', function($scope, $http, $cookies, $base64, $location) {
+    if($location.path() === '/signout') $cookies.jwt = null;
+    //if(!$cookies.jwt || $cookies.jwt.length >= 10) return $location.path('/preworkout');
+
+    if($location.path() === '/signup') $scope.newuser = true;
+
     $scope.login = function() {
-      $http.defaults.headers.common['Authentication'] = 'Basic ' + $base64.encode($scope.user.username + ':' + $scope.user.password);
+      $http.defaults.headers.common['Authorization'] = 'Basic ' + $base64.encode($scope.user.username + ':' + $scope.user.password);
       $http({
         method: 'GET',
-        url: '/api/v1/users',
-      }).success(function(data) {
-        $cookies.jwt = data.jwt;
-        $location.path('/preworkout');
-      }).error(function(data) {
-        console.log(data);
-      });
-    }
-
-    $scope.createNewUser = function() {
-      console.log('clicked');
-      $http({
-        method: 'POST',
-        url: '/api/v1/users',
-        data: $scope.user
+        url: '/api/v_0_0_1/users'
       })
       .success(function(data) {
         $cookies.jwt = data.jwt;
@@ -30,5 +23,25 @@ module.exports = function(app) {
       });
     };
 
+    $scope.validatePassword = function() {
+      return $scope.user.password === $scope.user.passwordConfirmation;
+    };
+
+    $scope.createNewUser = function() {
+      console.log('clicked');
+      $http({
+        method: 'POST',
+        url: '/api/v_0_0_1/users',
+        data: $scope.user
+      })
+      .success(function(data) {
+        $cookies.jwt = data.jwt;
+        $location.path('/preworkout');
+      })
+      .error(function(data) {
+        console.log('error');
+        console.log(data);
+      });
+    };
   });
-}
+};
--- app/js/controllers/preWorkoutController.js
+++ app/js/controllers/preWorkoutController.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
   app.controller('PreWorkoutController', function($scope, $http, $cookies) {
+  	console.log($cookies.jwt);
     $http.defaults.headers.common['jwt'] = $cookies.jwt;
   });
 }
